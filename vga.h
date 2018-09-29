562d9e6e15d89ff28adb3fe54d069d15f5623f78
Thomas R. Dial <dialtr@google.com>
2018-09-29 13:02:32 -0400

    added Makefile target to enable for consistent reformatting of the codebase according to Google conventions.

--- vga.h
+++ vga.h
@@ -7,49 +7,37 @@
 #ifndef VGA_H_INCLUDED
 #define VGA_H_INCLUDED
 
-#include <stdint.h>
 #include <stddef.h>
-
+#include <stdint.h>
 
 // The screen dimensions in standard VGA mode.
-enum {
-  VGA_WIDTH = 80,
-	VGA_HEIGHT = 25
-};
-
+enum { VGA_WIDTH = 80, VGA_HEIGHT = 25 };
 
 // VGA color constants.
 enum vga_color {
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15
+  VGA_COLOR_BLACK = 0,
+  VGA_COLOR_BLUE = 1,
+  VGA_COLOR_GREEN = 2,
+  VGA_COLOR_CYAN = 3,
+  VGA_COLOR_RED = 4,
+  VGA_COLOR_MAGENTA = 5,
+  VGA_COLOR_BROWN = 6,
+  VGA_COLOR_LIGHT_GREY = 7,
+  VGA_COLOR_DARK_GREY = 8,
+  VGA_COLOR_LIGHT_BLUE = 9,
+  VGA_COLOR_LIGHT_GREEN = 10,
+  VGA_COLOR_LIGHT_CYAN = 11,
+  VGA_COLOR_LIGHT_RED = 12,
+  VGA_COLOR_LIGHT_MAGENTA = 13,
+  VGA_COLOR_LIGHT_BROWN = 14,
+  VGA_COLOR_WHITE = 15
 };
-
 
 // VGA error codes
-enum {
-	VGA_SUCCESS = 0,
-	VGA_NOT_INITIALIZED = 1,
-	VGA_INVALID_DIMENSION = 2
-};
-
+enum { VGA_SUCCESS = 0, VGA_NOT_INITIALIZED = 1, VGA_INVALID_DIMENSION = 2 };
 
 // Status type returned by VGA driver functions.
 typedef int32_t vga_status_t;
-
 
 // Initialize the VGA driver and clear the screen.
 //
@@ -64,7 +52,6 @@ typedef int32_t vga_status_t;
 // in the library return a vga_status_t.
 vga_status_t vga_init();
 
-
 // Clear the screen buffer.
 //
 // Returns VGA_SUCCESS on success.
@@ -75,7 +62,6 @@ vga_status_t vga_init();
 // space characters that have a light grey foreground color and a black
 // background color.
 vga_status_t vga_clear();
-
 
 // Fill a rectangle in the buffer with the specified code point, attributes.
 //
@@ -90,12 +76,8 @@ vga_status_t vga_clear();
 // The function will not attempt to perform a fill if any argument to the
 // function is invalid; it will either perform the fill as requested or it
 // will return an error without writing to the screen buffer.
-vga_status_t vga_fill_rect(size_t x,
-		                       size_t y,
-									         size_t width,
-									         size_t height,
-									         unsigned char cp,
-									         enum vga_color fgcolor,
-									         enum vga_color bgcolor);
+vga_status_t vga_fill_rect(size_t x, size_t y, size_t width, size_t height,
+                           unsigned char cp, enum vga_color fgcolor,
+                           enum vga_color bgcolor);
 
 #endif  // VGA_H_INCLUDED
